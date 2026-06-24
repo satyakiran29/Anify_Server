b933139290017bb44b167d5e459a951299d8ef6e
Satyakiran <87798342+satyakiran29@users.noreply.github.com>
2026-06-25 01:21:05 +0530

    Update uploadKwgt.js

--- src/middleware/uploadKwgt.js
+++ src/middleware/uploadKwgt.js
@@ -15,9 +15,7 @@ const storage = multer.diskStorage({
     cb(null, UPLOAD_DIR);
   },
   filename: (req, file, cb) => {
-    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
-    const ext = path.extname(file.originalname).toLowerCase();
-    cb(null, `kwgt-${uniqueSuffix}${ext}`);
+    cb(null, file.originalname);
   }
 });
 
